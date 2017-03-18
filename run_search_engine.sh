94e858b1497785c6e94fb4a2d1b8d44aeef847d3
LilKdl <lucasrg@kth.se>
2017-03-18 19:53:49 +0100

    organization

--- run_search_engine.sh
+++ run_search_engine.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp classes:pdfbox:lib/gson-2.8.0.jar -Xmx3g ir.SearchGUI -d davisWiki -l ir17.gif -p patterns.txt
+java -cp classes:pdfbox:lib/gson-2.8.0.jar -Xmx3g ir.SearchGUI -d daviswiki -l ir17.gif -p patterns.txt
